e806c08bfc49b994483dee80cc7ffc1f95a46d25
JakeSmokie <jakesmokie@gmail.com>
2018-03-19 12:41:08 +0300

    eng desc

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //Хранимый на армейском складе боеприпас
+            // Хранимый на армейском складе боеприпас
+            // The ammunition stored in the army warehouse
         }
     }
 }
