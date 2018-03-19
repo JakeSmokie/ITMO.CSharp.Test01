5ee2181d5e83fe6ac9ddf70ded5605e915d2112d
JakeSmokie <jakesmokie@gmail.com>
2018-03-19 12:31:12 +0300

    ammocontainer

--- Test1/AmmoContainer.cs
+++ Test1/AmmoContainer.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Test1
 {
     class AmmoContainer : IEnumerable
     {
+        private List<Ammo> warehouse;
+
+        public void Add(Ammo ammo)
+        {
+            warehouse.Add(ammo);
+        }
+        public void Remove(Ammo ammo)
+        {
+            warehouse.Remove(ammo);
+        }
+        public List<Ammo> Search(string name)
+        {
+            return warehouse.FindAll(a => a.Name == name);
+        }
+        public List<Ammo> Search(Caliber caliber)
+        {
+            return warehouse.FindAll(a => a.Caliber.Equals(caliber));
+        }
+        public List<Ammo> Search(DamageType damageType)
+        {
+            return warehouse.FindAll(a => a.DamageType == damageType);
+        }
+        public void Sort()
+        {
+            warehouse.Sort();
+        }
+
         public IEnumerator GetEnumerator()
         {
-            throw new NotImplementedException();
-        }
-    }
-
-    class AmmoEnumerator : IEnumerator
-    {
-        public object Current => throw new NotImplementedException();
-
-        public bool MoveNext()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Reset()
-        {
-            throw new NotImplementedException();
+            return warehouse.GetEnumerator();
         }
     }
 }
